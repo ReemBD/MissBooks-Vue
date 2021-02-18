2e4eabaa0ad79092c5095dacf41ed8b5438c04f4
Reem Ben David <reemile1996@gmail.com>
2021-02-19 01:57:07 +0200

    gh-pages

---
+++ precache-manifest.938bf6af9a4934a1db10bad72b2a9ad4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84345bcb6178298a9297",
+    "url": "/css/app.ef4c61e5.css"
+  },
+  {
+    "revision": "cf4c0cf1e3365692ad5f",
+    "url": "/css/chunk-vendors.361cd96d.css"
+  },
+  {
+    "revision": "c78477ecf5b2a3bc75c9f2fe15a626c6",
+    "url": "/fonts/ReggaeOne-Regular.c78477ec.ttf"
+  },
+  {
+    "revision": "2cc5edf0195e983299b2c33048291371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1156d55ed79d11450b7e",
+    "url": "/js/about.013e8340.js"
+  },
+  {
+    "revision": "84345bcb6178298a9297",
+    "url": "/js/app.041dd3e0.js"
+  },
+  {
+    "revision": "cf4c0cf1e3365692ad5f",
+    "url": "/js/chunk-vendors.69823e48.js"
+  },
+  {
+    "revision": "025823a6d9f304b127e70f0c2b04b0d5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
